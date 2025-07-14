f6ac8ba9ef80c5fc107d02ad02cd0d420a3be392
taufiqraw <waskitaufiq@gmail.com>
2025-07-14 13:59:18 +0700

    feat: added shader class

--- shader/.fs
+++ shader/.fs
@@ -1,8 +1,8 @@
 #version 330
 precision mediump float;
-in vec2 position;
-out vec4 color;
+
+in vec3 color;
 
 void main() {
-  color = vec4(position, 1.0, 1.0);
+  gl_FragColor = vec4(color, 1.0);
 }
